53fce8723a2ac7ce18f4e6c2b72408720fbc4409
Kunal Subhash Lawtawar <Kunal_Lawtawar@ad.infosys.com>
2017-09-24 09:23:56 -0500

    Signed-off-by: Kunal Subhash Lawtawar <Kunal_Lawtawar@ad.infosys.com>

---
+++ sample1/src/demo/User.java
@@ -0,0 +1,35 @@
+package demo;
+
+class User {
+	private static Object userRoll;
+	int id;
+	String name;
+	int salary;
+	int roll;
+	String school;
+	
+	User(int userId, String userName){
+		id = userId;
+		name = userName;		
+	}
+	
+	User(int userId, String userName, int userRoll ){
+		this(userId, userName);
+		roll = userRoll;	
+	}
+    
+	
+	User(int userId, String userName, int userRoll, int userSalary){
+	    this(userId, userName, userRoll);
+	    salary = userSalary;
+	
+	}
+	
+	 public static void main(String[] args) {
+		User instructor = new User(1002, "Dheeru", 1234, 50000);
+		System.out.println("Name: " + instructor.name);
+		System.out.println("ID: " + instructor.id);
+		System.out.println("Salary: " + instructor.salary);
+		System.out.println("ROLL: " + instructor.roll);
+	 }
+}
